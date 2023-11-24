e9c4ee48c5ae27f6feab42faff56af5f7720e9c1
ArthurEv <arthurev.sg034@academico.ifsul.edu.br>
2023-11-24 11:07:25 -0300

    update

--- API.py
+++ API.py
@@ -35,6 +35,12 @@ def obter_CursosMatriculados(id):
     cursos = obterCursosMatriculados(id)
     return jsonify(cursos)
 
+#endpoint para obter dados de um curso
+@app.route("/curso/<int:id>", methods=["GET"])
+def obter_curso(id):
+    curso = obterCurso(id)
+    return jsonify(curso)
+
 #endpoint para obter todas as aulas de um curso
 @app.route("/aulas/<int:idcurso>", methods=["GET"])
 def obter_Aulas(idcurso):
@@ -54,5 +60,112 @@ def cadastrar_aluno():
     aluno = request.json
     cadastrarAluno(aluno)
     return (jsonify({"mensagem":"Aluno cadastrado com sucesso"}))
+
+#endpoint para criar fórum
+@app.route("/forum", methods=["POST"])
+def criar_forum():
+    forum = request.json
+    criarForum(forum)
+    return (jsonify({"mensagem":"Fórum criado com sucesso"}))
+
+#endpoint para obter todos os fóruns
+@app.route("/forum", methods=["GET"])
+def obter_Foruns():
+    foruns = obterForuns()
+    return jsonify(foruns)
+
+#endpoint para obter um fórum
+@app.route("/forum/<int:id>", methods=["GET"])
+def obter_Forum(id):
+    forum = obterForum(id)
+    return jsonify(forum)
+
+#endpoint para obter todas as mensagens de um fórum
+@app.route("/mensagens/<int:idforum>", methods=["GET"])
+def obter_Mensagens(idforum):
+    mensagens = obterMensagens(idforum)
+    return jsonify(mensagens)
+
+#endpoint para cadastrar uma mensagem em um fórum
+@app.route("/mensagem", methods=["POST"])
+def cadastrar_mensagem():
+    mensagem = request.json
+    cadastrarMensagem(mensagem)
+    return (jsonify({"mensagem":"Mensagem cadastrada com sucesso"}))
+
+#endpoint para cadastrar uma matrícula em um curso
+@app.route("/matricula", methods=["POST"])
+def cadastrar_matricula():
+    matricula = request.json
+    cadastrarMatricula(matricula)
+    return (jsonify({"mensagem":"Matrícula cadastrada com sucesso"}))
+
+#endpoint para ver se aluno já está matriculado em um curso	
+@app.route("/matriculado/<int:idcurso>/<int:idaluno>", methods=["GET"])
+def esta_matriculado(idcurso,idaluno):
+    matricula = matriculado(idcurso,idaluno)
+    return jsonify(matricula)
+
+#endpoint para obter próxima aula de um curso
+@app.route("/proximaAula/<int:idcurso>/<int:numero>", methods=["GET"])
+def obter_proximaAula(idcurso,numero):
+    aula = proximaAula(idcurso,numero)
+    return jsonify(aula)
+
+#endpoint para atualizar cadastro de um aluno
+@app.route("/atualizarAluno/<int:idaluno>", methods=["PUT"])
+def atualizar_aluno(idaluno):
+    aluno = request.json
+    atualizarAluno(aluno,idaluno)
+    return (jsonify({"mensagem":"Aluno atualizado com sucesso"}))
+
+
+### página de administrador ###
+
+#endpoint para adicionar curso
+@app.route("/curso", methods=["POST"])
+def adicionar_curso():
+    curso = request.json
+    insereCurso(curso)
+    return (jsonify({"mensagem":"Curso adicionado com sucesso"})) 
+
+#endpoint para atualizar curso
+@app.route("/curso/<int:id>", methods=["PUT"])
+def atualizar_curso(id):
+    curso = request.json
+    atualizaCurso(curso,id)
+    return (jsonify({"mensagem":"Curso atualizado com sucesso"}))
+
+#endpoint para deletar curso
+@app.route("/curso/<int:id>", methods=["DELETE"])
+def deletar_curso(id):
+    deletaCurso(id)
+    return (jsonify({"mensagem":"Curso deletado com sucesso"}))
+
+#endpoint para adicionar aula
+@app.route("/aula", methods=["POST"])
+def adicionar_aula():
+    aula = request.json
+    insereAula(aula)
+    return (jsonify({"mensagem":"Aula adicionado com sucesso"})) 
+
+#endpoint para atualizar curso
+@app.route("/aula/<int:id>", methods=["PUT"])
+def atualizar_aula(id):
+    aula = request.json
+    atualizaAula(aula,id)
+    return (jsonify({"mensagem":"Aula atualizado com sucesso"}))
+
+#endpoint para deletar curso
+@app.route("/aula/<int:id>", methods=["DELETE"])
+def deletar_aula(id):
+    deletaAula(id)
+    return (jsonify({"mensagem":"Aula deletado com sucesso"}))
+
+#endpoint para obter todos os professor
+@app.route("/professores", methods=["GET"])
+def obter_Professores():
+    professores = obterProfessores()
+    return jsonify(professores)
 
 app.run()
--- operacoes_banco.py
+++ operacoes_banco.py
@@ -41,7 +41,7 @@ def obter3CursosMatriculados(idaluno):
 def obterCursosDisponiveis():
     conexao = obterConexao()
     cursor = conexao.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM curso")
+    cursor.execute("SELECT curso.titulo,curso.descricao,curso.id,curso.quantidade_aulas,curso.idade,curso.idprofessor,professor.nome FROM curso INNER JOIN professor ON curso.idprofessor = professor.id")
     cursos = cursor.fetchall()
     conexao.close()
     return cursos
@@ -54,6 +54,15 @@ def obterCursosMatriculados(idaluno):
     cursos = cursor.fetchall()
     conexao.close()
     return cursos
+
+#Obtém informações de um curso
+def obterCurso(idcurso):
+    conexao = obterConexao()
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute("SELECT curso.titulo, curso.descricao, curso.id, professor.nome, professor.email FROM projetos.curso AS curso INNER JOIN projetos.professor AS professor ON curso.idprofessor = professor.id WHERE curso.id = %s",(idcurso,))
+    curso = cursor.fetchall()
+    conexao.close()
+    return curso
 
 #Obtém todas as aulas de um curso
 def obterAulas(idcurso):
@@ -68,7 +77,7 @@ def obterAulas(idcurso):
 def obterDadosAula(idaula):
     conexao = obterConexao()
     cursor = conexao.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM aula WHERE id = %s", (idaula,))
+    cursor.execute("SELECT aula.id, aula.titulo, aula.descricao, aula.video, aula.numero, aula.idcurso, curso.quantidade_aulas FROM aula INNER JOIN curso ON aula.idcurso = curso.id WHERE aula.id = %s", (idaula,))
     aula = cursor.fetchone()
     conexao.close()
     return aula
@@ -81,3 +90,143 @@ def cadastrarAluno(aluno):
     cursor.execute(sql, [aluno['nome'], aluno['email'], aluno['senha'], aluno['nascimento'], aluno['responsavel'], aluno['avatar']])
     conexao.commit()
     conexao.close()
+
+#Criar fórum
+def criarForum(forum):
+    conexao = obterConexao()
+    cursor = conexao.cursor()
+    sql = "INSERT INTO forum (titulo,assunto,idaluno) VALUES (%s,%s,%s)"
+    cursor.execute(sql, [forum['titulo'], forum['assunto'], forum['idaluno']])
+    conexao.commit()
+    conexao.close()
+
+#Obtem todos os fóruns
+def obterForuns():
+    conexao = obterConexao()
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM forum ORDER BY id DESC")
+    foruns = cursor.fetchall()
+    conexao.close()
+    return foruns
+
+#Obtem dados de um fórum
+def obterForum(idforum):
+    conexao = obterConexao()
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute("SELECT forum.titulo, forum.assunto, forum.datahora, aluno.avatar, aluno.nome FROM projetos.forum AS forum INNER JOIN projetos.aluno AS aluno ON forum.idaluno = aluno.id WHERE forum.id = %s", (idforum,))
+    forum = cursor.fetchone()
+    conexao.close()
+    return forum
+
+#Obtem todas as mensagens de um fórum
+def obterMensagens(idforum):
+    conexao = obterConexao()
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute("SELECT mensagem.mensagem, mensagem.datahora, aluno.avatar, aluno.nome FROM projetos.mensagem_forum AS mensagem INNER JOIN projetos.aluno AS aluno ON mensagem.idaluno = aluno.id WHERE mensagem.idforum = %s", (idforum,))
+    mensagens = []
+    mensagens = cursor.fetchall()
+    conexao.close()
+    return mensagens
+
+#Cadastra mensagem em um fórum
+def cadastrarMensagem(mensagem):
+    conexao = obterConexao()
+    cursor = conexao.cursor()
+    sql = "INSERT INTO mensagem_forum (mensagem,idforum,idaluno) VALUES (%s,%s,%s)"
+    cursor.execute(sql, [mensagem['mensagem'], mensagem['idforum'], mensagem['idaluno']])
+    conexao.commit()
+    conexao.close()
+
+#Cadastra matrícula em um curso
+def cadastrarMatricula(matricula):
+    conexao = obterConexao()
+    cursor = conexao.cursor()
+    sql = "INSERT INTO matricula (idcurso,idaluno) VALUES (%s,%s)"
+    cursor.execute(sql, [matricula['idcurso'], matricula['idaluno']])
+    conexao.commit()
+    conexao.close()
+
+#Busca se o aluno já está matriculado em um curso
+def matriculado(idcurso,idaluno):
+    conexao = obterConexao()
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM matricula WHERE idcurso = %s AND idaluno = %s", (idcurso,idaluno))
+    matricula = cursor.fetchall()
+    conexao.close()
+    return matricula
+
+#Busca a próxima aula de um curso
+def proximaAula(idcurso,numero):
+    conexao = obterConexao()
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM aula WHERE idcurso = %s AND numero > %s LIMIT 1", (idcurso,numero))
+    aula = cursor.fetchone()
+    conexao.close()
+    return aula
+
+#Atualiza cadastro do aluno
+def atualizarAluno(aluno,idaluno):
+    conexao = obterConexao()
+    cursor = conexao.cursor(dictionary=True)
+    sql = "UPDATE aluno SET senha = %s, avatar = %s WHERE id = %s"
+    cursor.execute(sql, [aluno['senha'], aluno['avatar'], idaluno])
+    conexao.commit()
+    conexao.close()
+
+
+#### Página de administrador ####
+
+def insereCurso(curso):
+    conexao = obterConexao()
+    cursor = conexao.cursor()  
+    sql = "INSERT INTO curso (titulo,descricao,idade,quantidade_aulas,idprofessor) VALUES (%s,%s,%s,%s,%s)"
+    cursor.execute(sql, [curso['titulo'], curso['descricao'], curso['idade'], curso['quantidade_aulas'], curso['idprofessor']])
+    conexao.commit()
+
+def atualizaCurso(curso,id):
+    conexao = obterConexao()
+    cursor = conexao.cursor(dictionary=True)
+    sql = "UPDATE curso SET titulo = %s, descricao = %s, idade = %s, quantidade_aulas = %s, idprofessor = %s WHERE id = %s"
+    cursor.execute(sql, [curso['titulo'], curso['descricao'], curso['idade'], curso['quantidade_aulas'], curso['idprofessor'], id])
+    conexao.commit()
+    conexao.close()
+
+def deletaCurso(id):
+    conexao = obterConexao()
+    cursor = conexao.cursor(dictionary=True)
+    sql = "DELETE FROM curso WHERE id = %s"
+    cursor.execute(sql, [id])
+    conexao.commit()
+    conexao.close()
+
+def insereAula(aula):
+    conexao = obterConexao()
+    cursor = conexao.cursor()  
+    sql = "INSERT INTO aula (titulo,descricao,video,numero,idcurso) VALUES (%s,%s,%s,%s,%s)"
+    cursor.execute(sql, [aula['titulo'], aula['descricao'], aula['video'], aula['numero'], aula['idcurso']])
+    conexao.commit()
+
+def atualizaAula(aula,id):
+    conexao = obterConexao()
+    cursor = conexao.cursor(dictionary=True)
+    sql = "UPDATE aula SET titulo = %s, descricao = %s, video = %s, numero = %s, idcurso = %s WHERE id = %s"
+    cursor.execute(sql, [aula['titulo'], aula['descricao'], aula['video'], aula['numero'], aula['idcurso'], id])
+    conexao.commit()
+    conexao.close()
+
+def deletaAula(id):
+    conexao = obterConexao()
+    cursor = conexao.cursor(dictionary=True)
+    sql = "DELETE FROM aula WHERE id = %s"
+    cursor.execute(sql, [id])
+    conexao.commit()
+    conexao.close()
+
+#Obtém todos os professores
+def obterProfessores():
+    conexao = obterConexao()
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM professor")
+    professores = cursor.fetchall()
+    conexao.close()
+    return professores
